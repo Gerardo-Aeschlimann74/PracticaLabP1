dc3117a7b0f858d4aedc60ac64420cb12e3c0b45
elcho <elcho@LAPTOP-RDCES2UH>
2022-04-22 15:15:53 -0600

    El anexo de la clase Alumno

---
+++ src/practica/lapp1/PracticaLapP1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.lapp1;
+
+/**
+ *
+ * @author elcho
+ */
+public class PracticaLapP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Caracteres Especiales
+        \n = Salto de línea
+        */
+      System.out.println("Hola Mundo "+(10+12.34)+"$ ");
+      System.out.print("Datos de alumnos");
+    }
+    
+}
